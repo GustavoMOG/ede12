1ec9424defbae4019f03f08bef77d58f0e50b0ad
GustavoMOG <gustavomog@hotmail.com>
2015-06-29 17:02:48 -0500

    first commit

---
+++ einstaller/einstall.h
@@ -0,0 +1,29 @@
+/*
+ * $Id: einstall.h 1654 2006-06-09 13:44:34Z karijes $
+ *
+ * Einstaller, frontend for package installation
+ * Part of Equinox Desktop Environment (EDE).
+ * Copyright (c) 2000-2006 EDE Authors.
+ *
+ * This program is licenced under terms of the 
+ * GNU General Public Licence version 2 or newer.
+ * See COPYING for details.
+ */
+
+#ifndef _EINSTALL_H
+#define _EINSTALL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <efltk/Fl_Check_Button.h>
+#include <efltk/filename.h>
+#include <efltk/fl_ask.h>
+#include <efltk/Fl_Util.h>
+#include <efltk/Fl_Locale.h>
+
+void install_package();
+
+#endif
+
+
---
+++ erun/erun.h
@@ -0,0 +1,23 @@
+/*
+ * $Id: erun.h 1654 2006-06-09 13:44:34Z karijes $
+ *
+ * Part of Equinox Desktop Environment (EDE).
+ * Copyright (c) 2000-2006 EDE Authors.
+ *
+ * This program is licenced under terms of the 
+ * GNU General Public Licence version 2 or newer.
+ * See COPYING for details.
+ */
+
+#ifndef erun_h
+#define erun_h
+#include <efltk/Fl.h>
+#include <efltk/Fl_Locale.h>
+#include <efltk/Fl_Window.h>
+extern Fl_Window* windowRunDialog;
+#include <efltk/Fl_Input.h>
+extern Fl_Input* inputRunDialog;
+#include <efltk/Fl_Box.h>
+#include <efltk/Fl_Button.h>
+void run_resource(char *cmd);
+#endif
